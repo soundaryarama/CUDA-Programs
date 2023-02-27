e9e26ba4d3e9335498443a233e9f3d2a7d419678
soundaryarama <113553039+soundaryarama@users.noreply.github.com>
2023-02-27 14:26:30 -0600

    Add files via upload

---
+++ Matrix Multiplication/task1.cu
@@ -0,0 +1,112 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <time.h>
+#include <chrono>
+#include <stdlib.h>
+#include "matmul.cuh"
+
+int main(int argc, char *argv[]){
+
+    // 
+    std::size_t n; std::size_t threads;
+    n = std::atoi(argv[1]); 
+    threads = std::atoi(argv[2]);
+    //n = 4;
+    
+    //float A[n*n];
+	//float B[n*n]; 
+    //float result[n*n];
+    srand(time(NULL));
+    // Check
+    /*
+    float A[n*n] = {5,2,6,1,0,6,2,0,3,8,1,4,1,8,5,6};
+    float B[n*n] = {7,5,8,0,1,8,2,6,9,4,3,8,5,3,7,9};*/
+
+    int size = (n*n)*sizeof(float);
+    float* result = (float*) malloc((n*n)*sizeof(int));
+    float* A = (float*) malloc((n*n)*sizeof(int));
+    float* B = (float*) malloc((n*n)*sizeof(int));
+
+     for(int i=0; i<n*n; i++){
+
+        A[i]= (float)(rand()) / float(RAND_MAX) * (2) - 1;
+        B[i]= (float)(rand()) / float(RAND_MAX) * (2) - 1;
+     }
+
+     float *dA; float *dB; float *dC;
+
+    // Allocate space for device
+    cudaMalloc((void **)&dA, size);
+    cudaMalloc((void **)&dB, size);
+    cudaMalloc((void **)&dC, size);
+
+    cudaMemcpy(dA, A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dB, B, size, cudaMemcpyHostToDevice);
+
+    
+
+    cudaEvent_t start;
+    cudaEvent_t stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    matmul(dA,dB,dC,n,threads);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    //Copy result from device to host
+    cudaMemcpy(result, dC, size, cudaMemcpyDeviceToHost);
+
+    // Get the elapsed time in milliseconds
+    float ms = 0.f;
+    cudaEventElapsedTime(&ms, start, stop);
+ 
+    printf("%f ", result[(n*n)-1]); printf("\n");
+    printf("%f ", ms); printf("\n");
+    /*
+    printf("A is  ");
+    for(int i = 0; i < n*n; ++i){
+        
+        printf("%f ", A[i]);
+    }
+    printf ("\n");
+    printf("B is  ");
+     for(int i = 0; i < n*n; ++i){
+        
+        
+        printf("%f ", B[i]);
+    }
+    printf ("\n");
+    printf("C is  ");
+     for(int i = 0; i < n*n; ++i){
+        
+        
+        printf("%f ", result[i]);
+    }
+    printf ("\n");
+
+    //Check if answer is correct
+    printf("CHECK: \n");
+    printf("FIRST ELEMENT: ");
+    float value = 0;
+    for (int i = 0; i < n; i++){
+        float first = A[i];
+        float second = B[i*n];
+        value += first*second; 
+    }
+    printf("%f \n", value);
+
+    printf("LAST ELEMENT: ");
+    float value1 = 0;
+    for (int i = 0; i < n; i++){
+        float first = A[n*n - n + i];
+        float second = B[(n-1)+i*n];
+        value1 += first*second; 
+    }
+    printf("%f \n", value1);*/
+    return 0;
+}
